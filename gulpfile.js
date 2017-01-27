20b13a5c1e606ee400bdab9af6d4680e887f230d
tawshif <anikphp@gmail.com>
2017-01-27 10:36:35 +0600

    some updates

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,8 @@ var runSequence = require('run-sequence');
 // Start browserSync server
 gulp.task('browserSync', function() {
     browserSync({
+        notify: true,
+        port: 5000,
         server: {
             baseDir: 'app'
         }
@@ -27,12 +29,15 @@ gulp.task('browserSync', function() {
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+
+        .pipe(sourcemaps.init())
         .pipe(plumber(function(error) {
             console.log("Error happend!", error.message);
             this.emit('end');
         }))
         .pipe(sass()) // Passes it through a gulp-sass
         .pipe(plumber.stop())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/css')) // Outputs it in the css folder
         .pipe(browserSync.reload({ // Reloading with Browser Sync
             stream: true
