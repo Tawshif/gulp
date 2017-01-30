6e9b3f6ff1b9bf329d215acd18bf326596c39823
tawshif <anikphp@gmail.com>
2017-01-30 17:45:44 +0600

    hanges

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,63 @@
 var data = '';
-    url = 'http://localhost:3000/db';
-    text = '';
+url = 'http://localhost:3000/db';
+text = '';
+table = document.getElementById('table-data');
 
-var table = document.getElementById('table-data');
 
-function loadData() {
-    var xhttp = new XMLHttpRequest();
-    
-    xhttp.onreadystatechange = function(){
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            data = JSON.parse(this.responseText).data;
-            console.log(data);
-            for (var i = 0; i < data.length; i++) {
-                text += "<tr>";
-                text += "<td>" + data[i].id + "</td>";
-                text += "<td>" + data[i].first_name + "</td>";
-                text += "<td>" + data[i].last_name + "</td>";
-                text += "<td>" + data[i].phone + "</td>";
-                text += "</tr>";
-            }
-            table.innerHTML = text;
-        }else{
-            console.log()
-        }
+
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", url, true);
+// xhr.onload = function(e) {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//         // response found
+//         if (xhr.status === 200) {
+//             // response OK
+//             console.log(xhr.statusText);
+//             data = JSON.parse(xhr.responseText).data;
+//             setTable(data);
+//             rowCount(table);
+//         } else {
+//             // response error e.g., 404
+//             console.error(xhr.status);
+//         }
+//     }
+// }
+// xhr.onerror = function(e) {
+//     console.error(xhr.statusText);
+// }
+// xhr.send(null);
+
+function setTable(data) {
+    for (var i = 0; i < data.length; i++) {
+        text += "<tr>";
+        text += "<td>" + data[i].id + "</td>";
+        text += "<td>" + data[i].first_name + "</td>";
+        text += "<td>" + data[i].last_name + "</td>";
+        text += "<td>" + data[i].phone + "</td>";
+        text += "</tr>";
     }
-    xhttp.open("GET", url , true);
-    xhttp.send();
-};
+    table.innerHTML = text;
+}
 
-loadData();
+// function rowCount(table) {
+//     console.log(table.getElementsByTagName("tr").length);
+// }
+
+
+$.ajax({
+    url:"http://localhost:3000/db",
+    type: 'GET',
+    dataType: 'json'
+})
+.done(function(data) {
+    setTable(data.data);
+})
+.fail(function(data) {
+    console.log(data);
+})
+.always(function(data) {
+    console.log("Success");
+});
+
+ajaxGetJSON(url, headers)
