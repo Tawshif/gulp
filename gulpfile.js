ef8dd910b9fc1de281e925234c9fbd7e2b9cd326
tawshif <anikphp@gmail.com>
2017-02-01 13:52:51 +0600

    bunch of bootstrap card design developed

--- gulpfile.js
+++ gulpfile.js
@@ -29,15 +29,15 @@ gulp.task('browserSync', function() {
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-
         .pipe(sourcemaps.init())
         .pipe(plumber(function(error) {
             console.log("Error happend!", error.message);
             this.emit('end');
         }))
         .pipe(sass()) // Passes it through a gulp-sass
-        .pipe(plumber.stop())
+        .pipe(autoprefixer())
         .pipe(sourcemaps.write())
+        .pipe(plumber.stop())    
         .pipe(gulp.dest('app/css')) // Outputs it in the css folder
         .pipe(browserSync.reload({ // Reloading with Browser Sync
             stream: true
